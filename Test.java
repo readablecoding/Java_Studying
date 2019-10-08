8fad750ae86c43b9a3a3fe4639cc83ac199f718b
readablecoding <>
2019-10-08 19:13:04 +0900

    19.10.8 화

---
+++ Test.java
@@ -0,0 +1,21 @@
+/**
+document 생성용 주석
+클래스, 변수, 메서드에 대한 설명
+*/
+	
+class Test 
+{
+	public static void main(String[] args) 
+	{
+		/*
+		여러 줄 
+		주석
+		*/
+		
+		int a = 10; //한 줄 주석
+		int b, c;
+		b = 5;
+		c = a + b;
+		System.out.println(c);
+	}
+}
---
+++ Test2.java
@@ -0,0 +1,9 @@
+class Test2   
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("What about My time??!!");
+		System.out.println(12345);
+
+	}
+}
