a74a8e9a241535263f75edf95abbd95f28bd3be2
readablecoding <>
2019-11-12 21:34:06 +0900

    19.11.12 화

--- Human2.java
+++ Human2.java
@@ -1,7 +1,7 @@
 interface Human				// 인터페이스, 객체 생성을 못함, 상수와 추상메소드로만 이루어짐
 {	
 	public static final int a = 0;	// 메모리에 하나만 생기고 Human.a 하면 상수 0을 리턴함
-	protected abstract void say(); // 추상 메소드, 하위클래스에서 반드시 오버라이딩 해야함
+	public abstract void say(); // 추상 메소드, 하위클래스에서 반드시 오버라이딩 해야함
 	
 }// interface
 
